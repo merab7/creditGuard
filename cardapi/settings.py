c4672c19cb04433348494b3315a9d1051f827a1b
MERAB TODUA <merabtodua7@gmail.com>
2024-06-05 08:16:21 +0200

    migratipns issue

--- cardapi/settings.py
+++ cardapi/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cards',
     'rest_framework',
     'django_filters',
+    'cards',
 
 ]
 
